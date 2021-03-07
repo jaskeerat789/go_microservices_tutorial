5b27ae5bb35eab1af8694f0647483222996d5bfd
Jaskeerat Singh <jaskee789@gmail.com>
2021-03-08 00:57:27 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module microservice_tutorial
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		// log.Println("hello world")/
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+		}
+		fmt.Fprintf(rw, "Hello %s \n", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("goodbye world")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
