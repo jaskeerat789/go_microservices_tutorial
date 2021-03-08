1921cd350fe3938684e22d2e1de4b4b9bcab1e6b
Jaskeerat Singh <jaskee789@gmail.com>
2021-03-09 02:09:32 +0530

    feat(handlers): add handler package

---
+++ handlers/hello.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+type Hello struct {
+	l *log.Logger
+}
+
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	h.l.Println("hello world")
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Oops", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(rw, "Hello %s \n", d)
+}
